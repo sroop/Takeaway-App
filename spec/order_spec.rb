eab948d6c73ef56351f8413f136c716f28e5820e
Sroop <sroopsunar@hotmail.com>
2014-04-28 20:58:35 +0100

    Order created empty and with the ability to add dishes

---
+++ spec/order_spec.rb
@@ -0,0 +1,18 @@
+require 'order'
+
+describe 'Order' do
+	
+	let(:salmon_sashimi) { double :dish, name: "Salmon Sashimi", price: 11 }
+	let(:order) { Order.new }
+
+	it 'has no dishes when created' do
+		expect(order.dishes).to be_empty
+	end
+
+	it 'can add a dish and specify the quantity' do
+		expect(order.dishes).to be_empty
+		order.add(salmon_sashimi)
+		expect(order.dishes).to eq([salmon_sashimi])
+	end
+
+end
